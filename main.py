e15f332ea13c47467639c0baf6ee44164969cbc2
nikkyxxx <savinnv1970@gmail.com>
2022-06-16 10:36:59 +0300

    Create parsing site

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import time
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import lxml
 
 
-# Press the green button in the gutter to run the script.
+def get_source_html(url):
+    driver = webdriver.Chrome(
+        executable_path=r"D:\project\pythonStudy\Chrome\chromedriver.exe"
+    )
+    driver.maximize_window()
+    try:
+        driver.get(url=url)
+        driver.implicitly_wait(5)
+        with open(f"source.html", "w", encoding="utf-8") as file:
+            file.write(driver.page_source)
+    except Exception as ex:
+        print(ex)
+    finally:
+        driver.close()
+        driver.quit()
+
+
+def get_items(file_path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        src = file.read()
+
+    soup = BeautifulSoup(src, "lxml")
+    items = soup.find_all("div", class_="start-search")
+
+    for item in items:
+        print(item)
+
+
+def main():
+    get_source_html("https://spb.zoon.ru")
+    get_items("source.html")
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
